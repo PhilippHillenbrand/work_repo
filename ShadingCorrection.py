606eae47edccca9d3a2d2a1cbca3a9b8daacfac9
Philipp <phillenb@students.uni-mainz.de>
2024-07-29 11:35:51 +0200

    Shading Correction Skript

---
+++ ShadingCorrection.py
@@ -0,0 +1,110 @@
+#%%
+import subprocess
+import sys
+import os
+from PIL import Image
+import numpy as np
+from basicpy import BaSiC
+from matplotlib import pyplot as plt
+
+os.environ["JAX_PLATFORM_NAME"] = "cpu"
+
+def run_command(command):
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode != 0:
+        print(f"Error running command: {command}\n{result.stderr.decode()}")
+    else:
+        print(result.stdout.decode())
+
+run_command(f"{sys.executable} -m pip uninstall -yq basicpy")
+run_command(f"{sys.executable} -m pip install --upgrade -q basicpy jax jaxlib")
+
+def load_tif(path):
+    img = Image.open(path)
+    images = []
+    for i in range(img.n_frames):
+        img.seek(i)
+        images.append(np.array(img))
+    return np.stack(images)
+
+images = load_tif('C:/Users/49157/work/wetransfer_test_2024-07-26_0808/test/143_signal_plane 2000-2009/15-27-55_143_640_l35_e75_r10164_st3_x4_z1_dhf7_Blaze[00 x 00]_C00_xyz-Table Z0000.ome-stitched_C01_T00000_Z02000_brighter.tif')
+images = load_tif('Path/To/.tif')
+
+basic = BaSiC(get_darkfield=True, smoothness_flatfield=1)
+
+try:
+    basic.fit(images)
+except Exception as e:
+    print(f"Error during fitting: {e}")
+
+if basic.baseline is None:
+    print("Baseline is None")
+else:
+    print("Baseline is computed:", basic.baseline)
+
+if basic.baseline is not None:
+    fig, axes = plt.subplots(1, 3, figsize=(9, 3))
+    im = axes[0].imshow(basic.flatfield)
+    fig.colorbar(im, ax=axes[0])
+    axes[0].set_title("Flatfield")
+    im = axes[1].imshow(basic.darkfield)
+    fig.colorbar(im, ax=axes[1])
+    axes[1].set_title("Darkfield")
+    axes[2].plot(basic.baseline)
+    axes[2].set_xlabel("Frame")
+    axes[2].set_ylabel("Baseline")
+    fig.tight_layout()
+    plt.show()
+else:
+    print("Skipping plot as baseline is None")
+
+images_transformed = basic.transform(images)
+
+def save_as_tiff(image_array, path):
+    img = Image.fromarray(image_array)
+    img.save(path, format='TIFF')
+
+save_as_tiff(images_transformed[0], 'test_corrected.tiff')
+
+i = 0
+fig, axes = plt.subplots(1, 2, figsize=(6, 3))
+im = axes[0].imshow(images[i])
+fig.colorbar(im, ax=axes[0])
+axes[0].set_title("Original")
+im = axes[1].imshow(images_transformed[i])
+fig.colorbar(im, ax=axes[1])
+axes[1].set_title("Corrected")
+fig.tight_layout()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
